5cd0c4b22606906a503e19ba1e31550bdc457349
Fule <laki.linux@gmail.com>
2021-09-26 16:53:39 -0700

    npm install client package.json  again and again

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,56 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  // query part
   Query: {
-    users: async () => {
-      return User.find().select('-__v -password');
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          '-__v -password'
+        );
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
     },
-    // books query part
-    books: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Book.find(params).sort({ createdAt: -1 });
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect user credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect user credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBookd: content } },
+          { new: true }
+        );
+
+        return user;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,45 +3,42 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     bookCount: Int
-    //arrays of the Book type
-    savedBooks:[Book]
-}
-type Book {
+    savedBooks: [Book]
+  }
+  type Book {
     bookId: String
-    authors: [authors]
+    authors: [String]
     description: String
     title: String
     image: String
     link: String
-}
-type Auth {
+  }
+  type Auth {
     token: ID!
     user: User
-}
-type Query {
-    me: User
-}
-type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email:String!, password: String!): Auth
-    saveBook (book: BookData): User
-    deleteBook (bookId: String): User
-}
- // book authors, description, title, bookId, image, and link input
-
-input BookData {
-    authors: [authors]
+  }
+  input BookData {
+    authors: [String]
     description: String
     title: String
     bookId: String
     image: String
     link: String
-}
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookData): User
+    deleteBook(bookId: String): User
+  }
 `;
 
 // export the typeDefs
