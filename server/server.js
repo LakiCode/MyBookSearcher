3eb809101c60afb25e99144853478a8d6a43aa03
Fule <laki.linux@gmail.com>
2021-09-26 02:19:38 -0700

    add auth to server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { ApolloServer } = require('apollo-server-express');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3002;
