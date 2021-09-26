1aea50bbfd2392099c97c829dff0210770348b0e
Fule <laki.linux@gmail.com>
2021-09-26 00:06:50 -0700

    add typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,10 +23,25 @@ type Auth {
     token: ID!
     user: User
 }
+type Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email:String!, password: String!): Auth
+    saveBook (book: BookData): User
+    deleteBook (bookId: String): User
+}
+ // book authors, description, title, bookId, image, and link input
 
-
-
-
+input BookData {
+    authors: [authors]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+}
 `;
 
 // export the typeDefs
