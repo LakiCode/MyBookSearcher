f9f7d89f673613336ae9b44d19184c52fb935f00
Fule <laki.linux@gmail.com>
2021-09-27 01:31:04 -0700

    savebook error

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -23,7 +23,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-  const [bookSaved] = useMutation(SAVE_BOOK);
+  const [saveBook] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -75,9 +75,11 @@ const SearchBooks = () => {
     }
 
     try {
-      await bookSaved({
-        variables: { book: bookToSave },
+      await saveBook({
+        variables: { bookData: { ...bookToSave } },
       });
+      // log book data
+      console.log(bookToSave);
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
