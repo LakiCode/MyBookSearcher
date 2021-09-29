b8e5e6bebc40ad9c9699c9a6a6332d4ee9260431
Fule <laki.linux@gmail.com>
2021-09-28 22:40:08 -0700

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -34,6 +34,10 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
