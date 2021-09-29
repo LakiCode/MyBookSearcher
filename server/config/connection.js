5ae110b6536c50acd48f5561b518da766ccadef0
Fule <laki.linux@gmail.com>
2021-09-29 01:22:46 -0700

    deploy to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/myBookSearcher',
+  process.env.MONGODB_URI || 'mongodb://localhost/mybooksearch',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
