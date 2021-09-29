e89fc79c917108a76f4bbe95ac2f315012e0a763
Fule <laki.linux@gmail.com>
2021-09-29 00:58:44 -0700

    add path

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3002;
@@ -34,8 +35,12 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 db.once('open', () => {
